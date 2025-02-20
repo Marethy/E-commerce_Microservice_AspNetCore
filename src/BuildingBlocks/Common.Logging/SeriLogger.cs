6de7353a3f49d289a1efb8efe998afcac44a2bc1
Marethy <khiemmm2004@gmail.com>
2025-02-20 16:50:47 +0700

    add serilogger

--- src/BuildingBlocks/Common.Logging/SeriLogger.cs
+++ src/BuildingBlocks/Common.Logging/SeriLogger.cs
@@ -1,6 +1,24 @@
-﻿namespace Common.Logging
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Common.Logging
 {
-    public class SeriLogger
+    public static class SeriLogger
     {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+            (context, configuration) =>
+            {
+                var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-");
+                var environmentName = context.HostingEnvironment.EnvironmentName?? "Development";
+
+                configuration
+                    .WriteTo.Debug()
+                    .WriteTo.Console(
+                        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}{NewLine}")
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("Environment", environmentName)
+                    .Enrich.WithProperty("Application", applicationName)
+                    .ReadFrom.Configuration(context.Configuration);
+            };
     }
 }
--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,25 +1,42 @@
+using Serilog;
+using Common.Logging;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+builder.Host.UseSerilog(SeriLogger.Configure);
+Log.Information("Starting Basket.API");
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Stopping Basket.API");
+    Log.CloseAndFlush();
+}
