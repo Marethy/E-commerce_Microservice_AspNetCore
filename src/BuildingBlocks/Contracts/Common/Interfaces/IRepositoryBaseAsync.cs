9135befea57e5169a4de7f1c0be45c73570307d1
Marethy <khiemmm2004@gmail.com>
2025-02-25 16:20:26 +0700

    Product API: init entity, dtos, repository pattern

---
+++ src/BuildingBlocks/Contracts/Common/Interfaces/IRepositoryBaseAsync.cs
@@ -0,0 +1,40 @@
+﻿using Contracts.Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
+
+namespace Contracts.Common.Interfaces
+{
+    // Interface cho các thao tác truy vấn dữ liệu
+    public interface IRepositoryQueryBase<T, K, TContext>
+        where T : EntityBase<K>
+        where TContext : DbContext
+    {
+        IQueryable<T> FindAll(bool trackChanges = false);
+        IQueryable<T> FindAll(bool trackChanges = false, params Expression<Func<T, object>>[] includeProperties);
+
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<T?> GetByIdAsync(K id);
+        Task<T?> GetByIdAsync(K id, params Expression<Func<T, object>>[] includeProperties);
+    }
+
+    // Interface cho các thao tác cơ bản của Repository (bao gồm cả truy vấn)
+    public interface IRepositoryBaseAsync<T, K, TContext> : IRepositoryQueryBase<T, K, TContext>
+        where T : EntityBase<K>
+        where TContext : DbContext
+    {
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> SaveChangesAsync();
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        Task EndTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
+
+
+
+
