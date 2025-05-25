ef9954d5a1d07e3b3bb6eb562853b73b09b73d42
Marethy <khiemmm2004@gmail.com>
2025-05-26 00:54:00 +0700

    chore: add dto and enum

---
+++ src/Saga.Orchestrator/HttpRepository/Interfaces/IOrderHttpRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ordering.Application.Common.Models;
+using Shared.DTOs.Order;
+
+namespace Saga.Orchestrator.HttpRepository.Interfaces;
+
+public interface IOrderHttpRepository
+{
+    Task<long> CreateOrderAsync(CreateOrderDto order);
+
+    Task<OrderDto> GetOrderAsync(long id);
+
+    Task<bool> DeleteOrderAsync(long id);
+
+    Task<bool> DeleteOrderByDocumentNo(string documentNo);
+}
