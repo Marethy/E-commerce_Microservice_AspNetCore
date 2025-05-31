6bee9e08dd96be05d1287cd446f8b3c9b7bd702a
Marethy <khiemmm2004@gmail.com>
2025-05-31 22:18:41 +0700

    feat(resilience-observability): containerize web health status

--- src/WebApps/WebHealthStatus/Controllers/HomeController.cs
+++ src/WebApps/WebHealthStatus/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using WebHealthStatus.Models;
 
 namespace WebHealthStatus.Controllers
 {
@@ -18,15 +17,6 @@ namespace WebHealthStatus.Controllers
             return Redirect("/healthchecks-ui");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+ 
     }
 }
